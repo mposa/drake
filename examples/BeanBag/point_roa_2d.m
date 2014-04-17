82611981179583c78fa43c5f507ddf40d464616b
mposa <mposa@mit.edu>
2014-04-17 16:46:13 -0400

    Added 2-d point example.

--- examples/BeanBag/point_roa_2d.m
+++ examples/BeanBag/point_roa_2d.m
@@ -15,7 +15,6 @@ switch sos_option
     PSD_fun = @newPSD;
   case 2
     sos_fun = @spot_mosek_sdsos;
-%     sos_fun = @spot_gurobi_sdsos;
     withSOS_fun = @withSDSOS;
     PSD_fun = @newSDD;
   case 3
@@ -27,53 +26,47 @@ end
 
 z = msspoly('z',1);
 zd = msspoly('zd',1);
+lx = msspoly('lx',1);
+xd = msspoly('xd',1);
 
 prog = spotsosprog();
 prog = prog.withIndeterminate(z);
 prog = prog.withIndeterminate(zd);
+prog = prog.withIndeterminate(xd);
+prog = prog.withIndeterminate(lx);
 
 degree = 4;
-x_vars = [z;zd];
+x_vars = [z;xd;zd];
 V_monoms = monomials([x_vars],1:degree);
 % V_monoms = V_monoms(2:end-4);
 [prog,V,coefv]= prog.newFreePoly(V_monoms);
 
-% load tmp
-% I = cv~=0;
-% cv(I) = cv(I) + .1*randn(sum(I),1);
-% cv = cv + .1*randn(length(cv),1);
-% V = cv'*V_monoms;
-% load tmp2
-% V = Vsol;
+E = .5*zd^2 + .5*xd^2 + g*z;
 
-E = .5*zd^2 + g*z;
+% V = (E + E^2) + .5*(zd^3 + zd*z) + xd^2*zd;
 
-% V = (E + E^2) + .5*(zd^3 + zd*z);
-
-% V = 5*E +  4*z^2 + zd^2*(zd^2+zd+1 ) + z*( 4*zd^2+(5/2)*zd+2 );
-
-% V = E + 5*E^2;
 
 % SOS equations
 % (1) V >= 0 for q admissable
 % (2) Vdot_free <= 0 for q admissable
 % (3) Vdot_impact <= 0 for q,lx admissable, phi=0, phidot<=0
 
-f_free = [zd;-g];
-f_impact = [0;1];
+f_free = [zd;0;-g];
+f_impact = [0;lx;1];
 
-Vdot_free = diff(V,[z;zd])*f_free;
-Vdot_impact = diff(V,[z;zd])*f_impact;
+Vdot_free = diff(V,[z;xd;zd])*f_free;
+Vdot_impact = diff(V,[z;xd;zd])*f_impact;
 
-alph = .1*(z^2+zd^2);
+alph = .1;
 
-sos_1 = V - alph;
-sos_2 = -Vdot_free - alph;
-sos_3 = -Vdot_impact - alph;
+sos_1 = V - alph*(z + xd^2+ zd^2);
+sos_2 = -Vdot_free - alph*(z + xd^2 + zd^2);
+sos_3 = -Vdot_impact - alph*(z + xd^2 + zd^2);
 
 %% Add in constraints
 phi = z;
 phidot = zd;
+psi = xd;
 
 const_deg = degree;
 sig = {};
@@ -88,16 +81,19 @@ const2_monoms = monomials(x_vars,0:degree);
 % const2_monoms = const2_monoms(1:end-1);
 % const2_monoms = const2_monoms([1:7 10:end]);
 
-const3_monoms = monomials(x_vars,0:degree);
+const3_monoms = monomials([x_vars;lx],0:degree);
 % const3_monoms = const3_monoms(1:end-1);
 
 % non-penetration (admissability of x)
 [prog, sos_1, sig{end+1}, coefsig{end+1}] = spotless_add_sprocedure(prog, sos_1, phi, [x_vars], const1_monoms, sos_option);
 [prog, sos_2, sig{end+1}, coefsig{end+1}] = spotless_add_sprocedure(prog, sos_2, phi, [x_vars], const2_monoms, sos_option);
-[prog, sos_3, sig{end+1}, coefsig{end+1}] = spotless_add_eq_sprocedure(prog, sos_3, phi, [x_vars], const_deg);
+[prog, sos_3, sig{end+1}, coefsig{end+1}] = spotless_add_eq_sprocedure(prog, sos_3, phi, [x_vars;lx], const_deg);
 
 % phidot <= 0 contact constraint
-[prog, sos_3, sig{end+1}, coefsig{end+1}] = spotless_add_sprocedure(prog, sos_3, -phidot, [x_vars], const3_monoms, sos_option);
+[prog, sos_3, sig{end+1}, coefsig{end+1}] = spotless_add_sprocedure(prog, sos_3, -phidot, [x_vars;lx], const3_monoms, sos_option);
+[prog, sos_3, sig{end+1}, coefsig{end+1}] = spotless_add_sprocedure(prog, sos_3, -psi*lx, [x_vars;lx], const3_monoms, sos_option);
+[prog, sos_3, sig{end+1}, coefsig{end+1}] = spotless_add_sprocedure(prog, sos_3, 1-lx^2, [x_vars;lx], const3_monoms, sos_option);
+[prog, sos_3, sig{end+1}, coefsig{end+1}] = spotless_add_eq_sprocedure(prog, sos_3, psi*(1-lx^2), [x_vars;lx], const_deg);
 
 %% Solve program
 prog = withSOS_fun(prog,sos_1);
@@ -106,8 +102,8 @@ prog = withSOS_fun(prog,sos_3);
 
 options = spotprog.defaultOptions;
 options.verbose = 1;
-options.do_fr = false;
-sol = prog.minimize(0,sos_fun,options);
+options.do_fr = true;
+sol = prog.minimize(0,@spot_mosek,options);
 
 
 % %%
